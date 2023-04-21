0293c4fe3fbafd8b3b8bae6420bff8f75ce3f218
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2023-04-21 09:54:40 +0900

    fix: remove unnecessary import in app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,30 +4,8 @@ import { AppService } from './app.service';
 import { UserController } from './controller/users/users.controller';
 import { UserRepositoryModule } from './infrastructure/repositories/user/user-repository.module';
 import { UserUseCaseModule } from './application/usecases/user/user-usecase.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'ddd_user',
-      password: 'password',
-      database: 'ddd_verification',
-      // entities,
-      logging: true,
-      synchronize: false,
-      cache: {
-        type: 'ioredis', // ioredisをキャッシュストアとして使用
-        options: {
-          host: 'localhost',
-          port: 6379,
-        },
-      },
-    }),
-    UserRepositoryModule,
-    UserUseCaseModule,
-  ],
+  imports: [UserRepositoryModule, UserUseCaseModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserUseCaseModule],
 })
